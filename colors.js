0596e57748fcfaa16272672580d9f886ab843497
Owen Chanthala <owenchanthala@gmail.com>
2020-01-31 22:46:22 -0800

    working repo

---
+++ colors.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = {
+  red: {
+    bodyBackground: "grey",
+    cardBackground: "red",
+    textColor: "black",
+    borderColor: "black"
+  },
+  orange: {
+    bodyBackground: "grey",
+    cardBackground: "orange",
+    textColor: "black",
+    borderColor: "black"
+  },
+  yellow: {
+    bodyBackground: "grey",
+    cardBackground: "yellow",
+    textColor: "black",
+    borderColor: "black"
+  },
+  green: {
+    bodyBackground: "grey",
+    cardBackground: "green",
+    textColor: "white",
+    borderColor: "white"
+  },
+  blue: {
+    bodyBackground: "grey",
+    cardBackground: "blue",
+    textColor: "white",
+    borderColor: "white"
+  },
+  purple: {
+    bodyBackground: 'grey',
+    cardBackground: 'purple',
+    textColor: 'white',
+    borderColor: 'white'
+  },
+  pink: {
+    bodyBackground: "grey",
+    cardBackground: "pink",
+    textColor: "black",
+    borderColor: "black"
+  },
+  black: {
+    bodyBackground: 'grey',
+    cardBackground: 'black',
+    textColor: 'white',
+    borderColor: 'white'
+  },
+  grey: {
+    bodyBackground: 'grey',
+    cardBackground: 'grey',
+    textColor: 'white',
+    borderColor: 'white'
+  }
+};
+
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const util = require("util");
+const colors = require("./colors");
 const writeFileAsync = util.promisify(fs.writeFile);
 let actualName;
 let userImg;
@@ -11,114 +12,184 @@ let userBio;
 let userFollowers;
 let useFollowing;
 let userRepos;
-let pickedColor;
+let userStars;
+let userLocation;
+let locationLink;
 
 inquirer
   .prompt([
     {
+      type: "input",
       message: "Enter your GitHub username:",
       name: "username"
     },
     {
+      type: "list",
       message: "Enter a color:",
-      name: "color"
+      name: "color",
+      choices: [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "purple",
+        "pink",
+        "black",
+        "grey"
+      ]
     }
   ])
-  .then(({ username }) => {
-    const queryUrl = `https://api.github.com/users/${username}`;
+  .then(({ username, color }) => {
+    const gitUrl = `https://api.github.com/users/${username}`;
+
+    const starUrl = `https://api.github.com/users/${username}/starred`;
+
+    axios.get(starUrl).then(response => {
+      userStars = response.data.length;
+    });
 
     axios
-      .get(queryUrl)
+      .get(gitUrl)
       .then(response => {
-        console.log(response.data);
         actualName = response.data.name;
-        // console.log(actualName);
         userImg = response.data.avatar_url;
-        // console.log(userImg);
         userBio = response.data.bio;
-        // console.log(userBio);
+        userLocation = response.data.location;
+        locationLink = `https://www.google.com/maps/place/${userLocation}`;
         userFollowers = response.data.followers;
-        // console.log(userFollowers);
         useFollowing = response.data.following;
-        // console.log(useFollowing);
         userRepos = response.data.public_repos;
-        // console.log(userRepos);
       })
-      // .then(({ color }) => {
-      //   pickedColor = color;
-      // })
-      .then(function(response) {
-        const htmlVar = `<!DOCTYPE html>
-         <html lang="en">
-           <head>
-             <meta charset="UTF-8" />
-             <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-             <link
-               rel="stylesheet"
-               href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-             />
-             <title>Github Discovery</title>
-           </head>
-           <body>
-               <div class="container text-center">
-                   <div class="row">
-                       <div class="col-md-12">
-                         <div class="card" style="background-color:">
-                           <div class="card-body">
-                             <img src="${userImg}" height="250px" width="250px" class="img-responsive img-circle" style="border: solid; border-radius: 50%;">
-                             <h2>My name is ${actualName}</h2>
-                             <h2>This is my GitHub Overview.</h2>
-                             <p>${userBio}</p>
-                           </div>
-                         </div>
-                       </div>
-                   </div>
-               </div>
-             <div class="container text-center">
-               <div class="row" style="padding-top: 70px;">
-                 <div class="col-md-6">
-                   <div class="card">
-                     <div class="card-body">
-                       <h3 class="card-title">Followers</h3>
-                       <h4 class="card-text">${userFollowers}</h4>
-                     </div>
-                   </div>
-                 </div>
-                 <div class="col-md-6">
-                   <div class="card">
-                     <div class="card-body">
-                       <h3 class="card-title">Following</h3>
-                       <h4 class="card-text">${useFollowing}</h4>
-                     </div>
-                   </div>
-                 </div>
-               </div>
-               <div class="row" style="padding-top: 20px;">
-                   <div class="col-md-6">
-                     <div class="card">
-                       <div class="card-body">
-                         <h3 class="card-title">Public Repos</h3>
-                         <h4 class="card-text">${userRepos}</h4>
-                       </div>
-                     </div>
-                   </div>
-                   <div class="col-md-6">
-                     <div class="card">
-                       <div class="card-body">
-                         <h3 class="card-title">Github Stars</h3>
-                         <h4 class="card-text"></h4>
-                       </div>
-                     </div>
-                   </div>
-                 </div>
-             </div>
-           </body>
-         </html>`;
-        writeFileAsync("index.html", htmlVar, function(err) {
-          if (err) {
-            return console.error(err);
+      .then(function() {
+        makeHTML(
+          actualName,
+          userImg,
+          userBio,
+          userLocation,
+          userFollowers,
+          useFollowing,
+          userRepos,
+          userStars,
+          color
+        );
+        writeFileAsync(
+          "index.html",
+          makeHTML(
+            actualName,
+            userImg,
+            userBio,
+            userLocation,
+            userFollowers,
+            useFollowing,
+            userRepos,
+            userStars,
+            color,
+            locationLink
+          ),
+          function(err) {
+            if (err) {
+              return console.error(err);
+            } 
+            else {
+              return console.log("Discovered User Infos!");
+            }
           }
-          console.log("Discovered User Infos!");
-        });
+        );
       });
   });
+function makeHTML(
+  actualName,
+  userImg,
+  userBio,
+  userLocation,
+  userFollowers,
+  useFollowing,
+  userRepos,
+  userStars,
+  color,
+  locationLink
+) {
+  return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        />
+        <title>Github Discovery</title>
+        <style>
+        body{
+          background-color: ${colors[color].bodyBackground};
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+        a:hover{
+          opacity: 60%;
+        }
+        </style>
+      </head>
+      <body>
+          <div class="container text-center">
+              <div class="row">
+                  <div class="col-md-12">
+                    <div class="card" style="border: solid; border-color:${colors[color].borderColor}; color:${colors[color].textColor};">
+                      <div class="card-body" style="color:${colors[color].textColor}; background-color:${colors[color].cardBackground};">
+                        <img src="${userImg}" height="250px" width="250px" class="img-responsive img-circle" style="border: solid; border-radius: 50%; border-color:${colors[color].borderColor}">
+                        <h2>My name is ${actualName}</h2>
+                        <h2>This is my GitHub Overview.</h2>
+                        <a style="font-size: 32px; color: ${colors[color].textColor}; text-decoration: none;" href="${locationLink}" target="_blank">${userLocation}</a>
+                        <p>${userBio}</p>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+        <div class="container text-center">
+          <div class="row" style="padding-top: 70px;">
+            <div class="col-md-6">
+              <div class="card" style="border: solid; border-color:${colors[color].borderColor}; color:${colors[color].textColor};">
+                <div class="card-body" style="color:${colors[color].textColor}; background-color:${colors[color].cardBackground};">
+                  <h3 class="card-title">Followers</h3>
+                  <h4 class="card-text">${userFollowers}</h4>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card" style="border: solid; border-color:${colors[color].borderColor}; color:${colors[color].textColor};">
+                <div class="card-body" style="color:${colors[color].textColor}; background-color:${colors[color].cardBackground};">
+                  <h3 class="card-title">Following</h3>
+                  <h4 class="card-text">${useFollowing}</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row" style="padding-top: 20px;">
+              <div class="col-md-6">
+                <div class="card" style="border: solid; border-color:${colors[color].borderColor}; color:${colors[color].textColor};">
+                  <div class="card-body" style="color:${colors[color].textColor}; background-color:${colors[color].cardBackground};">
+                    <h3 class="card-title">Public Repos</h3>
+                    <h4 class="card-text">${userRepos}</h4>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="card" style="border: solid; border-color:${colors[color].borderColor}; color:${colors[color].textColor};">
+                  <div class="card-body" style="color:${colors[color].textColor}; background-color:${colors[color].cardBackground};">
+                    <h3 class="card-title">Github Stars</h3>
+                    <h4 class="card-text">${userStars}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <script
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+      ></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      </body>
+    </html>`;
+}
